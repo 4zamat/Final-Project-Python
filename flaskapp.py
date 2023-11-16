2a096e9b4559cb2828cb0e1cb4c97fc6a8ae257a
4zamat <53268129+4zamat@users.noreply.github.com>
2023-11-16 13:24:03 +0600

    Draft start
    Kickstart

---
+++ flaskapp.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+db = SQLAlchemy()
+
+# app object (literally web server)
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = r"postgresql://postgres:123@localhost:5432/cinema"
+
+with app.app_context():
+    db.init_app(app)
+
+app.config['SECRET_KEY']="my secret key here"
